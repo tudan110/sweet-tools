378023e83506dfc16772c97a0a52c294863285ad
tudan110 <wtudan@126.com>
2019-12-31 16:36:32 +0800

    添加 view-design；添加菜单示例

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,31 +1,123 @@
 <template>
   <div id="wrapper">
     <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
+    <!-- <system-information></system-information> -->
+    <Row>
+        <Col span="8">
+            <Menu :theme="theme2">
+                <Submenu name="1">
+                    <template slot="title">
+                        <Icon type="ios-paper" />
+                        内容管理
+                    </template>
+                    <MenuItem name="1-1">文章管理</MenuItem>
+                    <MenuItem name="1-2">评论管理</MenuItem>
+                    <MenuItem name="1-3">举报管理</MenuItem>
+                </Submenu>
+                <Submenu name="2">
+                    <template slot="title">
+                        <Icon type="ios-people" />
+                        用户管理
+                    </template>
+                    <MenuItem name="2-1">新增用户</MenuItem>
+                    <MenuItem name="2-2">活跃用户</MenuItem>
+                </Submenu>
+                <Submenu name="3">
+                    <template slot="title">
+                        <Icon type="ios-stats" />
+                        统计分析
+                    </template>
+                    <MenuGroup title="使用">
+                        <MenuItem name="3-1">新增和启动</MenuItem>
+                        <MenuItem name="3-2">活跃分析</MenuItem>
+                        <MenuItem name="3-3">时段分析</MenuItem>
+                    </MenuGroup>
+                    <MenuGroup title="留存">
+                        <MenuItem name="3-4">用户留存</MenuItem>
+                        <MenuItem name="3-5">流失用户</MenuItem>
+                    </MenuGroup>
+                </Submenu>
+            </Menu>
+        </Col>
+        <Col span="8">
+            <Menu :theme="theme2" active-name="1-2" :open-names="['1']">
+                <Submenu name="1">
+                    <template slot="title">
+                        <Icon type="ios-paper" />
+                        内容管理
+                    </template>
+                    <MenuItem name="1-1">文章管理</MenuItem>
+                    <MenuItem name="1-2">评论管理</MenuItem>
+                    <MenuItem name="1-3">举报管理</MenuItem>
+                </Submenu>
+                <Submenu name="2">
+                    <template slot="title">
+                        <Icon type="ios-people" />
+                        用户管理
+                    </template>
+                    <MenuItem name="2-1">新增用户</MenuItem>
+                    <MenuItem name="2-2">活跃用户</MenuItem>
+                </Submenu>
+                <Submenu name="3">
+                    <template slot="title">
+                        <Icon type="ios-stats" />
+                        统计分析
+                    </template>
+                    <MenuGroup title="使用">
+                        <MenuItem name="3-1">新增和启动</MenuItem>
+                        <MenuItem name="3-2">活跃分析</MenuItem>
+                        <MenuItem name="3-3">时段分析</MenuItem>
+                    </MenuGroup>
+                    <MenuGroup title="留存">
+                        <MenuItem name="3-4">用户留存</MenuItem>
+                        <MenuItem name="3-5">流失用户</MenuItem>
+                    </MenuGroup>
+                </Submenu>
+            </Menu>
+        </Col>
+        <Col span="8">
+            <Menu :theme="theme2" :open-names="['1']" accordion>
+                <Submenu name="1">
+                    <template slot="title">
+                        <Icon type="ios-paper" />
+                        内容管理
+                    </template>
+                    <MenuItem name="1-1">文章管理</MenuItem>
+                    <MenuItem name="1-2">评论管理</MenuItem>
+                    <MenuItem name="1-3">举报管理</MenuItem>
+                </Submenu>
+                <Submenu name="2">
+                    <template slot="title">
+                        <Icon type="ios-people" />
+                        用户管理
+                    </template>
+                    <MenuItem name="2-1">新增用户</MenuItem>
+                    <MenuItem name="2-2">活跃用户</MenuItem>
+                </Submenu>
+                <Submenu name="3">
+                    <template slot="title">
+                        <Icon type="ios-stats" />
+                        统计分析
+                    </template>
+                    <MenuGroup title="使用">
+                        <MenuItem name="3-1">新增和启动</MenuItem>
+                        <MenuItem name="3-2">活跃分析</MenuItem>
+                        <MenuItem name="3-3">时段分析</MenuItem>
+                    </MenuGroup>
+                    <MenuGroup title="留存">
+                        <MenuItem name="3-4">用户留存</MenuItem>
+                        <MenuItem name="3-5">流失用户</MenuItem>
+                    </MenuGroup>
+                </Submenu>
+            </Menu>
+        </Col>
+    </Row>
+    <br>
+    <p>Change theme</p>
+    <RadioGroup v-model="theme2">
+        <Radio label="light"></Radio>
+        <Radio label="dark"></Radio>
+    </RadioGroup>
   </div>
 </template>
 
@@ -35,6 +127,11 @@
   export default {
     name: 'landing-page',
     components: { SystemInformation },
+    data () {
+      return {
+        theme2: 'light'
+      }
+    },
     methods: {
       open (link) {
         this.$electron.shell.openExternal(link)
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,9 +5,13 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import ViewUI from 'view-design'
+import 'view-design/dist/styles/iview.css'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+Vue.use(ViewUI)
 
 /* eslint-disable no-new */
 new Vue({
