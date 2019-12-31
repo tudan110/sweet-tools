5d083d36e4f7aa741f0e401865e65abd6772f321
tudan110 <wtudan@126.com>
2019-12-31 15:58:24 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'sweet-tools'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
